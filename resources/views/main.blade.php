e354a7ce46be58b2b73fce1bfa1f441b305de1bc
AndreaGir <girolamiandrea2000@gmail.com>
2022-04-23 23:49:08 +0200

    comm

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,6 +8,50 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
+
+    <div>
+      
+        <div class="primomain">
+          
+             <div class="dentromain">
+               
+                 <div class="scritta ">
+                   
+                    
+                    <div class="carta">
+                        @foreach($slides as $value)
+                        <div class="tuttaimg">
+                            
+                            <img class="immagine2" src="{{ $value ['thumb'] }}" alt="">
+                        </div>
+
+                        <h3>{{ $value ['series'] }}</h3>
+                        @endforeach
+                        
+                        
+                        
+                        
+                       
+                    </div>
+
+                 </div>
+                 <div class="lead">
+                   <h3>LEAD MORE</h3>
+                 </div>
+
+                 <div class="current">
+                   <h3>CURRENT SERIES</h3>
+                 </div>
+                  
+                  
+                 
+             </div>
+            
+          </div>
+
+          
+
+    </div>
     
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,8 @@
 <body>
 
     @include('header')
+
+    @include('main')
     
 </body>
 </html>
